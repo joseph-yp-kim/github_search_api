b34169b04e8f5a007ba8a289d5c7ec0dd1ffed5c
Joseph Kim <joseph.yp.kim@gmail.com>
2022-03-02 08:25:09 -0800

    Get request to github with access token qorking

--- server/index.js
+++ server/index.js
@@ -1,13 +1,32 @@
-const express = require("express");
-const https = require("https");
-console.log("https");
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
 
 const PORT = process.env.PORT || 3001;
+const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
 
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+app.get('/api/github/repos', async (req, res, next) => {
+  try {
+    const results = await axios({
+      method: 'get',
+      url: `https://api.github.com/users/joseph-yp-kim/repos`,
+      headers: {
+        Authorization: `Bearer ${GITHUB_ACCESS_TOKEN}`,
+        'Content-Type': 'application/json',
+        Accept: 'application/vnd.github.mercy-preview+json',
+      },
+    });
+    console.log('results;', results.data);
+    res.json(results.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from server!' });
 });
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
