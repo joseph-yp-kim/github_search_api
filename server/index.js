25e6afaf173bb5b67a13029c6c6065c47042a2cb
Joseph Kim <joseph.yp.kim@gmail.com>
2022-03-02 07:58:56 -0800

    Create initial app

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const https = require("https");
+console.log("https");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
