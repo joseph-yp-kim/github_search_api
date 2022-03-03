fd978c3b0127cdf96759a9cb79b503074dbac181
Joseph Kim <joseph.yp.kim@gmail.com>
2022-03-02 21:48:01 -0800

    Get basic search functionality working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,47 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { useState, useEffect } from 'react';
+import './App.css';
+import SearchResults from './SearchResults';
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [didAttemptSearch, setDidAttemptSearch] = useState(false);
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+  let searchTimeout;
+  const handleSearchInputChange = (e) => {
+    clearTimeout(searchTimeout);
+    const searchInputText = e.target.value;
+    searchTimeout = setTimeout(() => {
+      console.log('====>', searchInputText);
+      if (!searchInputText) {
+        setSearchResults([]);
+        setIsLoading(false);
+        setDidAttemptSearch(false);
+        return;
+      }
+      fetchRepos(searchInputText);
+    }, 500);
+  };
+
+  const fetchRepos = async (query) => {
+    setIsLoading(true);
+    setDidAttemptSearch(true);
+    const res = await fetch(`/api/github/repos?search=${query}`);
+    const results = await res.json();
+    setSearchResults(results);
+    setIsLoading(false);
+    console.log('results:', results);
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+    <div className='App'>
+      <header className='App-header'>
+        <input onChange={handleSearchInputChange}></input>
+        {isLoading && <span>loading results...</span>}
+        {!isLoading && didAttemptSearch && !searchResults.length && (
+          <span>Sorry... no matching results found</span>
+        )}
+        {!isLoading && <SearchResults repos={searchResults} />}
       </header>
     </div>
   );
---
+++ client/src/SearchResults.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const RepoItem = styled.li`
+  border: 1px solid black;
+  text-align: left;
+  margin-bottom: 10px;
+  padding: 10px;
+`;
+
+const RepoDescription = styled.p`
+  margin: 0px;
+`;
+
+function SearchResults({ repos }) {
+  const repoList = repos.map((repo, i) => (
+    <RepoItem key={i}>
+      <RepoDescription>
+        {repo.name} by {repo.owner}
+      </RepoDescription>
+      <RepoDescription>{repo.description}</RepoDescription>
+      <RepoDescription>Stars: {repo.starCount}</RepoDescription>
+    </RepoItem>
+  ));
+
+  return <ul>{repoList}</ul>;
+}
+
+export default SearchResults;
